cd01f7334a85b55a35a4267eae4d84b8c9129aa9
Pinja <pinskumelissa@gmail.com>
2018-10-08 10:35:39 +0300

    lisäsin maat ja arvot

--- src/blackjack/Kortti.java
+++ src/blackjack/Kortti.java
@@ -14,12 +14,19 @@ public class Kortti {
     //atribuutit
     private int arvo = 0;
     private String maa = "";
+    
+    private static int[] arvot = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+    private static String[] maat = {"Risti","Ruutu","Hertta","Pata"};
 
     //konstruktori
     public Kortti(int kortinArvo, String kortinMaa) {
         this.arvo = kortinArvo;
         this.maa = kortinMaa;
     }
-
+    
+    public String toString(){
+        return arvot[arvo]+", "+maat[maa];
+    }
+    
     //muut metodit
 }
