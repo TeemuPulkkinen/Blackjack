c29bd8ceb261300eadcf50ae14c40e016b92a348
Teemu Pulkkinen <pulkkinen.teemu@gmail.com>
2018-10-11 10:12:02 +0300

    Lisää metodeja.

--- src/blackjack/PelaajanKasi.java
+++ src/blackjack/PelaajanKasi.java
@@ -20,29 +20,38 @@ public class PelaajanKasi {
         int pelaajankasi = satunnaisKortti;
         return satunnaisKortti;
     }
-    
+
+    int arvo = 0;
+    int kortti1 = 0;
+    int kortti2 = 0;
+
     public int selvitaSumma(int kadenSumma) {
-        
-        int arvo = 0;
-        int kortti1 = 0;
-        int kortti2 = 0;
-        if (arvo >=2  || arvo <=10) {
-            
+
+        if (arvo >= 2 || arvo <= 10) {
+
             kadenSumma = kadenSumma + arvo;
         }
-        if (arvo >=11 || arvo <= 13) {
-            
+        if (arvo >= 11 || arvo <= 13) {
+
             kadenSumma = kadenSumma + 10;
         }
         if (arvo == 1) {
-            
+
             kadenSumma = kadenSumma + 11;
         }
         if (kortti1 + kortti2 == 21) {
-            
+
             kadenSumma = kadenSumma - 10;
         }
         return kadenSumma;
+    }
+
+    //selvittää onko pelaajalla blackjack
+    public void blackjack() {
+
+        if (kortti1 >= 10 && kortti1 <= 13 && kortti2 == 1) {
+            System.out.println("Sinulla on blackjack!");
+        }
     }
 
 }
